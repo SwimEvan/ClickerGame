2980525c6be95b2380fac5a18e58921eaead6abf
Evan Meyer <evan.meyer26@d155.org>
2024-09-26 07:25:22 -0500

    Initial Commit

---
+++ ClickerGameTests/ClickerGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerGameTests.swift
+//  ClickerGameTests
+//
+//  Created by EVAN MEYER on 9/26/24.
+//
+
+import Testing
+@testable import ClickerGame
+
+struct ClickerGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
