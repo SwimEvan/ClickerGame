170799f406f583d2012af1fb69248dc8fac0b635
Evan Meyer <evan.meyer26@d155.org>
2024-09-27 08:12:40 -0500

    hi

--- ClickerGame/StoreViewController.swift
+++ ClickerGame/StoreViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class StoreViewController: UIViewController {
 
+    
+    @IBOutlet weak var pointLabel: UILabel!
+    @IBOutlet weak var pickleJarEvaOutlet: UIImageView!
+    
+    @IBOutlet weak var pickleballEva: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        pickleJarEvaOutlet.image = UIImage(named: "FiveTimePickle")
+        pickleballEva.image = UIImage(named: "PickleballLoser")
+        pointLabel.text = "Pickles: \(AppData.points)"
+        
+      
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func PickleJarAction(_ sender: UIButton) {
+        if(AppData.points >= 100.0){
+            AppData.pickle = 2
+            AppData.points-=100
+        }
+        else{
+            pointLabel.text = "Error"
+        }
+
+        
+    }
+    
+    @IBAction func PickleballAction(_ sender: UIButton) {
+        if(AppData.points >= 750.0){
+            AppData.pickle = 3
+            AppData.points = 0
+        }
+        else{
+            pointLabel.text = "Error"
+        }
     }
     
 
--- ClickerGame/ViewController.swift
+++ ClickerGame/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
-
+class AppData{
+   static var pickle = 1
+   static var points = 0.0
+}
 class ViewController: UIViewController {
-
+    let image = UIImage(named: "StartPickle")
+    @IBOutlet weak var pickleLabel: UILabel!
+    @IBOutlet weak var PickleimageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        PickleimageView.image = UIImage(named: "StartPickle")
+      
+    
     }
+    override func viewDidAppear(_ animated: Bool) {
+        pickleLabel.text = "Pickles: \(AppData.points)"
+        if(AppData.pickle == 1){
+            print("1")
+            AppData.points = AppData.points + 1.0
+            pickleLabel.text = "Pickles: \(AppData.points)"
+        }
+        else if(AppData.pickle == 2){
+            print("2")
+            PickleimageView.image = UIImage(named: "FiveTimePickle")
+            AppData.points = AppData.points + 5.0
+            pickleLabel.text = "Pickles: \(AppData.points)"
+        }
+        else{
+            print("3")
+            PickleimageView.image = UIImage(named: "PickleballLoser")
+            AppData.points += 0.5
+            pickleLabel.text = "Pickles: \(AppData.points)"
+        }
+    }
+    
 
-
+    @IBAction func storeAction(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapButtonAction(_ sender: UIButton) {
+        print("button tapped")
+        if(AppData.pickle == 1){
+            print("1")
+            AppData.points = AppData.points + 1.0
+            pickleLabel.text = "Pickles: \(AppData.points)"
+        }
+        else if(AppData.pickle == 2){
+            print("2")
+            PickleimageView.image = UIImage(named: "FiveTimePickle")
+            AppData.points = AppData.points + 5.0
+            pickleLabel.text = "Pickles: \(AppData.points)"
+        }
+        else{
+            print("3")
+            PickleimageView.image = UIImage(named: "PickleballLoser")
+            AppData.points += 0.5
+            pickleLabel.text = "Pickles: \(AppData.points)"
+        }
+        
+        
+    }
+    
 }
 
